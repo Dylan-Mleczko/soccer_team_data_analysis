8766e6abce3a6e6f65f263d81b82f177af8d7864
Dylan-Mleczko <dcooksonmlec@student.unimelb.edu.au>
2021-08-31 11:34:23 +0000

    Improved code readability

--- assignment1.py
+++ assignment1.py
@@ -13,17 +13,17 @@ articlespath = 'data/football'
 
 def task1():
     with open(datafilepath) as file:
-        data = json.load(file)
-    return sorted(data["teams_codes"])
+        team_codes = json.load(file)["teams_codes"]
+    return sorted(team_codes)
     
 def task2():
     with open(datafilepath) as file:
         data = json.load(file)
-    club_data = {}
-    for club in data["clubs"]:
-        club_data[club["club_code"]] = [club["goals_scored"], club["goals_conceded"]]
-    csv_data = pd.DataFrame.from_dict(club_data, orient = "index", columns = ["goals_scored_by_team", "goals_scored_against_team"]).sort_index()
-    csv_data.to_csv("task2.csv", index_label = "club_code")
+    team_goals = {}
+    for team in data["clubs"]:
+        team_goals[team["club_code"]] = [team["goals_scored"], team["goals_conceded"]]
+    csv_data = pd.DataFrame.from_dict(team_goals, orient = "index", columns = ["goals_scored_by_team", "goals_scored_against_team"]).sort_index()
+    csv_data.to_csv("task2.csv", index_label = "team_code")
     return
       
 def task3():
@@ -31,78 +31,89 @@ def task3():
     for file in [file for file in os.listdir(articlespath) if file.endswith(".txt")]:
         with open(articlespath + "/" + file) as article:
             article = article.read()    
-        scores = re.findall(r"(\d+)-(\d+)", article)
-        largest_score = 0
-        for score in [score for score in scores if len(score[0]) <= 2 and len(score[1]) <= 2]:
+        article_scores = re.findall(r"(\d+)-(\d+)", article)
+        article_largest_score = 0
+        for score in [score for score in article_scores if len(score[0]) <= 2 and len(score[1]) <= 2]:
             score = int(score[0]) + int(score[1])
-            if score > largest_score:
-                largest_score = score
-        total_scores[file] = largest_score
+            if score > article_largest_score:
+                article_largest_score = score
+        total_scores[file] = article_largest_score
     pd.DataFrame.from_dict(total_scores, orient = "index", columns = ["total_goals"]).sort_index().to_csv("task3.csv", index_label = "filename")
     return
 
 def task4():
     plt.boxplot(pd.read_csv("task3.csv")["total_goals"])
-    plt.title("Distribution of Total Goals from Soccer Matches")
+    plt.title("Distribution of Total Scores from Soccer Matches", fontweight = "bold")
+    plt.ylabel("Soccer Match Total Score", fontweight = "bold")
     plt.xticks([])
+    plt.yticks(range(0, 101, 10))
+    plt.grid(True, which = "major", axis = "y")
     plt.savefig("task4.png")
+    plt.clf()
     return
     
 def task5():
     with open(datafilepath) as file:
-        clubs = json.load(file)["participating_clubs"]
-    club_mentions = {}
-    for club in clubs:
-        club_mentions[club] = 0
+        teams = json.load(file)["participating_clubs"]
+    team_mentions = {}
+    for team in teams:
+        team_mentions[team] = 0
     for file in [file for file in os.listdir(articlespath) if file.endswith(".txt")]:
         with open(articlespath + "/" + file) as article:
             article = article.read()
-        for club in clubs:
-            club_mentions[club] += club in article
-    pd.DataFrame.from_dict(club_mentions, orient = "index", columns = ["number_of_mentions"]).sort_index().to_csv("task5.csv", index_label = "club_name")
+        for team in teams:
+            team_mentions[team] += team in article
+    pd.DataFrame.from_dict(team_mentions, orient = "index", columns = ["number_of_mentions"]).sort_index().to_csv("task5.csv", index_label = "club_name")
     csv_data = pd.read_csv("task5.csv")
     plt.bar(csv_data["club_name"], csv_data["number_of_mentions"])
-    plt.title("Soccer Team Mentions in Media")
-    plt.xlabel("Team Name")
-    plt.ylabel("Number of Articles Mentioning Team at Least Once")
+    plt.title("Mentions for Soccer Teams in Articles", fontweight = "bold")
+    plt.xlabel("Team", fontweight = "bold")
+    plt.ylabel("Number of Articles Mentioning Team at Least Once", fontweight = "bold")
     plt.xticks(rotation = 90)
+    plt.yticks(range(0, 91, 10))
+    plt.grid(True, which = "major", axis = "y")
     plt.savefig("task5.png", bbox_inches = "tight")
+    plt.clf()
     return
     
 def task6():
     with open(datafilepath) as file:
-        clubs = json.load(file)["participating_clubs"]
-    single_club_mentions = [0 for i in range(len(clubs))]
-    pair_club_mentions = [len(clubs) * [0] for i in range(len(clubs))]
-    similarity_scores = [len(clubs) * [0] for i in range(len(clubs))]
+        teams = json.load(file)["participating_clubs"]
+    single_team_mentions = [0 for i in range(len(teams))]
+    pair_team_mentions = similarity_scores = [len(teams) * [0] for i in range(len(teams))]
     for file in [file for file in os.listdir(articlespath) if file.endswith(".txt")]:
         with open(articlespath + "/" + file) as article:
             article = article.read()
-        for i in range(len(clubs)):
-            if clubs[i] in article:
-                single_club_mentions[i] += 1
-                for j in range(i + 1, len(clubs)):
-                    if clubs[j] in article:
-                        pair_club_mentions[i][j] += 1
-                        pair_club_mentions[j][i] += 1
-    for i in range(len(clubs)):
-        for j in range(len(clubs)):
+        for i in range(len(teams)):
+            if teams[i] in article:
+                single_team_mentions[i] += 1
+                for j in range(i + 1, len(teams)):
+                    if teams[j] in article:
+                        pair_team_mentions[i][j] += 1
+                        pair_team_mentions[j][i] += 1
+    for i in range(len(teams)):
+        for j in range(len(teams)):
             if i == j:
                 similarity_scores[i][j] = similarity_scores[j][i] = 1
-            elif single_club_mentions[i] + single_club_mentions[j] > 0:
-                similarity_scores[i][j] = similarity_scores[j][i] = (2 * pair_club_mentions[i][j]) / (single_club_mentions[i] + single_club_mentions[j])
-    similarity_scores_dataframe = pd.DataFrame(similarity_scores, index = clubs, columns = clubs).sort_index(axis = 0).sort_index(axis = 1)
-    sns.heatmap(similarity_scores_dataframe, cbar_kws = {"label": "Similarity Score of Team Pair"}, cmap = "rainbow")
-    plt.title("Similarity in Media Mentions between Soccer Teams")
+            elif single_team_mentions[i] + single_team_mentions[j] > 0:
+                similarity_scores[i][j] = (2 * pair_team_mentions[i][j]) / (single_team_mentions[i] + single_team_mentions[j])
+    dataframe = pd.DataFrame(similarity_scores, index = teams, columns = teams).sort_index(axis = 0).sort_index(axis = 1)
+    sns.heatmap(dataframe, cbar_kws = {"label": "Team Pair Similarity Score"}, cmap = "rainbow")
+    plt.title("Similarity of Article Mentions between Soccer Teams", fontweight = "bold")
     plt.savefig("task6.png", bbox_inches = "tight")
+    plt.clf()
     return
     
 def task7():
     plt.scatter(pd.read_csv("task5.csv")["number_of_mentions"], pd.read_csv("task2.csv")["goals_scored_by_team"])
-    plt.title("Correlation between Soccer Team Mentions in Media and Goals")
-    plt.xlabel("Number of Articles Mentioning Team at Least Once")
-    plt.ylabel("Number of Total Goals Scored by Team")
-    plt.savefig("task7.png")
+    plt.title("Relationship between Mentions of Soccer Teams in Articles and Goals Scored", fontweight = "bold")
+    plt.xlabel("Number of Articles Mentioning Team at Least Once", fontweight = "bold")
+    plt.ylabel("Number of Total Goals Scored by Team", fontweight = "bold")
+    plt.xticks(range(0, 101, 10))
+    plt.yticks(range(0, 13, 1))
+    plt.grid(True)
+    plt.savefig("task7.png", bbox_inches = "tight")
+    plt.clf()
     return
     
 def task8(filename):
@@ -112,20 +123,20 @@ def task8(filename):
     
 def task9():
     transformer = TfidfTransformer()
-    article_names = [file for file in sorted(os.listdir(articlespath)) if ".txt" in file]
+    article_names = [file for file in sorted(os.listdir(articlespath)) if file.endswith(".txt")]
     articles = [task8(articlespath + "/" + article) for article in article_names]
-    terms = []
-    for article_terms in articles:
-        terms += article_terms
-    terms = sorted(list(set(terms)))
-    term_counts = [len(terms) * [0] for i in range(len(articles))]
+    all_terms = []
+    cosine_similarity_scores = []
+    for article in articles:
+        all_terms += article
+    all_terms = sorted(list(set(all_terms)))
+    term_counts = [len(all_terms) * [0] for i in range(len(articles))]
     for i in range(len(articles)):
-        for word in articles[i]:
-            term_counts[i][terms.index(word)] += 1
+        for term in articles[i]:
+            term_counts[i][all_terms.index(term)] += 1
     tfidf = transformer.fit_transform(term_counts).toarray()
-    similarity_scores = []
     for i in range(len(article_names)):
         for j in range(i + 1, len(article_names)):
-            similarity_scores += [[article_names[i], article_names[j], np.dot(tfidf[i], tfidf[j]) / (np.linalg.norm(tfidf[i]) * np.linalg.norm(tfidf[j]))]]
-    pd.DataFrame(similarity_scores, columns = ["article1", "article2", "similarity"]).nlargest(10, "similarity").to_csv("task9.csv", index = False)
+            cosine_similarity_scores += [[article_names[i], article_names[j], np.dot(tfidf[i], tfidf[j]) / (np.linalg.norm(tfidf[i]) * np.linalg.norm(tfidf[j]))]]
+    pd.DataFrame(cosine_similarity_scores, columns = ["article1", "article2", "similarity"]).nlargest(10, "similarity").to_csv("task9.csv", index = False)
     return
